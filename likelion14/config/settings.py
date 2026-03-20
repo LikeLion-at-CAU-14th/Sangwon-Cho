49c623f8040ee6f0e596847d04a8045e590823a1
sangwon5579 <chosw0410@cau.ac.kr>
2026-03-20 12:59:08 +0900

    fix: 중복 제거

--- likelion14/config/settings.py
+++ likelion14/config/settings.py
@@ -68,14 +68,7 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
