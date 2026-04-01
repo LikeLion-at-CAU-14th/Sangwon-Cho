67c3434fe73a234fbf2408e163d2a13a3c6e6c03
sangwon5579 <chosw0410@cau.ac.kr>
2026-04-01 20:04:24 +0900

    feat: 기본 CRUD 구현

--- likelion14/posts/urls.py
+++ likelion14/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('<int:id>', get_post_detail)
+    # path('', hello_world, name = 'hello_world'),
+    # path('page', index, name='my-page'),
+    # path('<int:id>', get_post_detail)
+
+    path('', post_list, name="post_list"),
+    path('<int:post_id>/', post_detail, name = "post_detail")
 ]
